7e060bfb44f9f95bd6c92edc08b7387086fcbf9c
Nadja Sarsur <nsarsur@thoughtworks.com>
2016-09-19 12:05:03 -0300

    First exercise done

---
+++ src/Triangle.java
@@ -0,0 +1,25 @@
+/**
+ * Created by nsarsur on 9/19/16.
+ */
+public class Triangle {
+
+    private int n;
+
+    Triangle(int n)
+    {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void easestExercise(){
+        System.out.print("*");
+    }
+    
+}
--- src/main.java
+++ src/main.java
@@ -1,9 +1,17 @@
 /**
  * Created by nsarsur on 9/19/16.
  */
+
+import java.util.Random;
+
+
 public class main {
 
-    System.out.println("Exercises TWU - Introductory programming exercises");
 
-
+    public static void main(String [] args) {
+        System.out.println("Exercises TWU - Introductory programming exercises");
+        Random rand = new Random();
+        Triangle triangle = new Triangle(rand.nextInt(10));
+        triangle.easestExercise();
+    }
 }
